a14b1c94d6c872f440791b3bb93f01858fc53530
srikpv <pillarisetty@outlook.com>
2021-02-28 22:36:03 -0500

    Node ENV

--- src/utils/DB.js
+++ src/utils/DB.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const url = (process.env.NODE_ENV === "development" ? "" : "https://dream11-project2.herokuapp.com");
 //const url = process.env.REACT_APP_API_URL;
-
+const myHeaders = new Headers({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  });
 // Export an object containing methods we'll use for accessing the random user API
 export default {
   getPlayers: function() {
     return axios
-    .get(url + "/api/all/players")
+    .get(url + "/api/all/players", {
+        headers : myHeaders,
+    })
       .then(res => {
         const players = res.data.players;
         return players.map(player => {
