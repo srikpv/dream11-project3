6d558a837a27d5f8c80937039da1a5996f614ebb
melvyn10 <melvyn.ten@ge.com>
2021-03-06 17:21:12 -0500

    working version

--- src/components/Play.js
+++ src/components/Play.js
@@ -3,35 +3,51 @@ import "./Play.css";
 import DB from "../utils/DB";
 
 function Play() {
-    // Pick teams to play a new game
-    const [teams, setTeams] = useState(
-        []);
+    /* Pick teams to play a new game */
     const [gameTeams, setGameTeams] = useState({
+        teams:[],
         homeTeam: "",
         oppTeam: ""
     });
     const [gameData, setGameData] = useState({
-        gameId: 1,
+        gameId:"",
+        winningTeamName:"",
         game: ""
     });
 
     useEffect(() => {
-        loadTeams()
-        loadGameData()
+        loadTeams();
     }, []);
+
+    function getTeamName(teamId) {
+        /* set the winning team name from team id */
+        let winnerName = "";
+        if (gameTeams.teams.length) {
+            console.log ("find winner ",teamId);
+            const winner = gameTeams.teams.find( ({ id }) => id == teamId );
+            console.log ("winner ",winner);
+            if (winner) {winnerName = winner.name};
+            return winnerName;
+        }
+    };
 
     function loadTeams() {
         console.log("call server API to get team");
         DB.getTeams()
         .then(res => {
             console.log ("team call return ", res.data.teams);
-            setTeams(res.data.teams);
+            setGameTeams({
+                teams:res.data.teams,
+                homeTeam:"",
+                oppTeam:""
+            });
             }
         )
         .catch(err => console.log(err));  
     };
 
     function handlePlayGame(event) {
+        /* handle when user selects 2 teams to play a game */
         event.preventDefault();
         DB.putNewGame({
             home_team_id:gameTeams.homeTeam,
@@ -39,37 +55,39 @@ function Play() {
         })
         .then(res => {
             console.log ("game id returned ",res.data.game.id);
-            setGameData({
-                gameId:res.data.game.id,
-                game: ""
-            });
-            console.log("gameData ",gameData);
+            loadGameData(res.data.game.id);
         })
         .then(() => {
             console.log ("reset game teams");
-            setGameTeams({
+            setGameTeams({...gameTeams,
             homeTeam: "",
             oppTeam: ""
             });
+            console.log ("reset game teams after ", gameTeams);
         })
-        .then(() => loadGameData())
         .catch(err => console.log(err));
     };
 
-    function loadGameData() {
-        console.log ("loading game data for game ",gameData.gameId);
-        (gameData.gameId ? (
-            DB.getGame(gameData.gameId)
+    function loadGameData(gameIdToLoad) {
+        console.log ("loading game data for game ",gameIdToLoad);
+        if (gameIdToLoad) {
+            DB.getGame(gameIdToLoad)
                 .then(res => {
-                    console.log("new garem data ",res.data.game);
-                    setGameData({...gameData,game:res.data.game});
+                    console.log("new game data ",res.data.game);
+                    const winningTeam = getTeamName(res.data.game.win_team_id);
+                    setGameData({gameId:gameIdToLoad,winningTeamName:winningTeam, game:res.data.game});
+                    console.log("set game data ",gameData);
                     }
                 )
-                .catch(err => console.log(err))
-            ) : ( 
-            console.log("no game played yet")
-            )             
-        );
+                .catch(err => console.log(err));
+        }
+        else {
+            console.log("no game played yet");
+        }
+    };
+
+    function resetGame() {
+        setGameTeams({...gameTeams, homeTeam: "", oppTeam: ""});
     }
 
 return (  
@@ -77,26 +95,32 @@ return (
         <div className="col-sm-4">
         <h5> Select 2 teams to play a game</h5>
         <form onSubmit={handlePlayGame}>
-            {teams.length ? (
+            {gameTeams.teams.length ? (
                 <div className="row">
                 <ul>
-                {teams.map(item => {
+                {gameTeams.teams.map(item => {
                     return (
-                        <div className="column">
-                            <label>{item.name}</label>
-                            <label  className="homeTxt-check-space">
-                            <input class="with-gap" type="radio" name="hometeam" className="home-check-space"
+                        <div className="row">
+                            <div className="col s3">
+                                <span>{item.name}</span>
+                            </div>
+                            <div className="col s3">
+                                <label  className="homeTxt-check-space">
+                                <input class="with-gap" type="radio" name="hometeam" className="home-check-space"
                                 value={item._id}
-                            onChange={e => setGameTeams({...gameTeams,homeTeam:item.id})}/>
-                            <span>Home </span>
-                            </label>
+                                onClick={e => setGameTeams({...gameTeams,homeTeam:item.id})}/>
+                                <span>Home </span>
+                                </label>
+                            </div>
 
-                            <label  className="oppTxt-check-space">
-                            <input class="with-gap" type="radio" name="oppoteam" className="opp-check-space"
+                            <div className="col s3">
+                                <label  className="oppTxt-check-space">
+                                <input class="with-gap" type="radio" name="oppoteam" className="opp-check-space"
                                 value={item._id}
-                            onChange={e => setGameTeams({...gameTeams,oppTeam:item.id})}/>
-                            <span> Opp </span>
-                            </label>
+                                onClick={e => setGameTeams({...gameTeams,oppTeam:item.id})}/>
+                                <span> Opp </span>
+                                </label>
+                            </div>
                         </div>
                     )
                 })}
@@ -110,12 +134,13 @@ return (
             </button>
         </form>
         </div>
-
+        <div className="row">
         <div className="col-sm-8">
             <h5> Game Results </h5>
+            <h5> Team {gameData.winningTeamName} Wins! </h5>
             {(gameData.gameId && gameData.game) ? (
                 <div className="row">
-                    <h3> Team {gameData.game.win_team_id} Wins! </h3>
+                    
                     <div className="col-lg-4">
                         <h5> Home Team </h5>
                     {gameData.game.home_team_players.length ? (
@@ -132,6 +157,7 @@ return (
                         <h6> No home team data </h6>
                     )}
                     </div>
+                    
                     <div className="col-lg-4">
                     <h5> Opposing Team </h5>
                     {gameData.game.opp_team_players.length ? (
@@ -154,7 +180,7 @@ return (
                 )}
         </div>               
     </div>
-    
+    </div>
 );
 }
 
