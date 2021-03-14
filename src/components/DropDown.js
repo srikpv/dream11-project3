e43d826c7da588ce25bd31ffb22044d20664dbaf
Rakesh <arorark77@gmail.com>
2021-03-14 03:29:45 -0400

    dropdown value sticky

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -4,11 +4,11 @@ function DropDown(props) {
   let options = props.options
   let optionItems = options.map((option) =>
   <option key={option.id} value={option.id}>{option.name}</option>)
-  console.log(optionItems)
+  // console.log(optionItems)
   return (
     // <select class="browser-default" value="" name={props.type} className="select-group" onChange={props.handleInputChange}>
-      <select class="browser-default" value="" name={props.type} onChange={props.handleInputChange}>
-      <option value="" disabled>Make Selection</option>
+      <select class="browser-default" value={options.name} name={props.type} onChange={props.handleInputChange}>
+      <option value="" enabled>Make Selection</option>
         {optionItems}        
     </select>
   );
